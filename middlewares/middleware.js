ba2a7c2f0f9568985c73f2ccb8bbbcaf0c808af1
agarwal-123 <43489651+agarwal-123@users.noreply.github.com>
2020-09-13 02:21:33 +0530

    Update middleware.js

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,20 +1,20 @@
 var session = require('express-session');
 const bcrypt = require('bcryptjs')
 const user = require('../models/user')
+const {SECRET_TOKEN_KEY}= require('../SECRET_KEYS')
 
 
 const jwt= require('jsonwebtoken')
 
 // A middleware to check the validity of the token recieved from the frontend in headers.
 
-/* The user ID and Token are stored in session storage ( Using security keys ),
-to reduce the repetetive fetching of user details from databse in furthur queries. */
+// The user ID and Token are stored in session storage ( Using security keys ) to to reduce the repetetive fetching of user details from databse in furthur queries.
 
 async function check(req, res, next) {
 	try{
 		const token = req.header('authorization').replace('Bearer ','')
 		
-		const decoded =jwt.verify(token,'aSecretKey')
+		const decoded =jwt.verify(token,SECRET_TOKEN_KEY)
 		var done=0
 		const tup= await user.findOne({_id: decoded._id})
 		
